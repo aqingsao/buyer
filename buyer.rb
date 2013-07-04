ce66f2fdaad0fb75bdd056ee65f522fa831b7338
aqingsao <vinci.zhang@gmail.com>
2013-07-04 15:40:53 +0800

    user could view product

--- buyer.rb
+++ buyer.rb
@@ -2,7 +2,7 @@ require 'rubygems'
 require File.join(File.dirname(__FILE__), 'user.rb')
 
 User.new(1234).login()
-User.new(1235).login()
+User.new(1235).shopping([1])
 
 #[InActiveUser.new(1234), InActiveUser.new(1235)].startShopping();
 #[InActiveUser.new(1234), InActiveUser.new(1235)].startShopping();
--- user.rb
+++ user.rb
@@ -15,7 +15,8 @@ class User
 		get("logout", {user: @id})
 	end
 
-	def view
+	def view(productId)
+          get(sprintf("products/%d", productId))
 	end
 
 	def compare
@@ -29,9 +30,14 @@ class User
 
 	def pay
 	end	
+        
+        def shopping(viewed, compared=[], added=[], bought=[], paid=[])
+          login
+          viewed.each{|p| view(p)}
+        end
 
 	private 
-	def get(url, parameters)
+	def get(url, parameters={})
 		url = HOST+url
 		url += ("?" + parameters.map{|k, v| "#{k}=#{v}"}.join("&")) unless parameters.empty?
 		@browser.get(url)
