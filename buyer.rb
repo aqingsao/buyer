81966d21965254ced98d9790930cb9d5372142cd
aqingsao <vinci.zhang@gmail.com>
2013-07-04 20:05:37 +0800

    add many users for each type

--- buyer.rb
+++ buyer.rb
@@ -1,20 +1,33 @@
 require 'rubygems'
 require File.join(File.dirname(__FILE__), 'user.rb')
 
-threads =[
-	Thread.new do
-		NonActiveUser.new(1230).doWork()
-	end, 
-	Thread.new do
-		PotentialUser.new(1231).doWork()
-	end, 
-	Thread.new do
-		ActiveUser.new(1232).doWork()
-	end, 
-	Thread.new do
-		VerifyActiveUser.new(1233).doWork()
-	end
-]
+@@userIndex = 0;
+def userId
+	sprintf("10%03d%03d", rand(1000), @@userIndex += 1).to_i
+end
+
+threads =[]
+# 100.times do 
+# 	threads << Thread.new do
+# 		NonActiveUser.new(userId()).doWork()
+# 	end 
+# end
+
+20.times do 
+	threads << Thread.new do
+		PotentialUser.new(userId()).doWork()
+	end 
+end
+# 10.times do 
+# 	threads << Thread.new do
+# 		ActiveUser.new(userId()).doWork()
+# 	end
+# end
+# 8.times do 
+# 	threads << Thread.new do
+# 		VerifyActiveUser.new(userId()).doWork()
+# 	end
+# end
 # t1.join
 threads.each {|t| t.join}
 #User.new(1235).do([{view:[1,2,3], action:{carted:[1], addOrder:[1], confirmOrder:[1], paid:[1]}}])
